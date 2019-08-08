b263564ea412677bf7399cf6a2abf5bef46aebf0
Mushtaq J <mushtaq.jameel@cambio.lk>
2019-08-08 16:17:48 +0530

    initial commit for creational  patterns

---
+++ creational-patterns/src/main/java/se/mushtaq/designpatterns/creational/Application.java
@@ -0,0 +1,13 @@
+package se.mushtaq.designpatterns.creational;
+
+/**
+ * <p>
+ * TODO - Explain What does this class do ?
+ * </p>
+ *
+ * @author MJameel
+ * @since 8/8/2019
+ **/
+public class Application
+{
+}
